e95e411c702f590f1a4e976a92461a33816daa34
Alexander <amarty777@gmail.com>
2023-04-16 08:49:56 +0300

    update module

--- projects/angular-phone-helper/src/lib/angular-phone-helper.module.ts
+++ projects/angular-phone-helper/src/lib/angular-phone-helper.module.ts
@@ -1,7 +1,7 @@
 import {ModuleWithProviders, NgModule} from '@angular/core';
 import {PhoneFormatPipe} from "./pipes/phone-format.pipe";
 import {CommonModule} from "@angular/common";
-import {ApiConfig} from "./models/config.model";
+import {ApiConfig, defaultConfig} from "./models/config.model";
 import {API_CONFIG_TOKEN} from "./di";
 import {PhoneFormatService} from "./phone-format.service";
 
@@ -18,11 +18,11 @@ import {PhoneFormatService} from "./phone-format.service";
 })
 
 export class AngularPhoneHelperModule {
-  static forRoot(config: ApiConfig): ModuleWithProviders<AngularPhoneHelperModule> {
+  static forRoot(config?: ApiConfig | null): ModuleWithProviders<AngularPhoneHelperModule> {
     return {
       ngModule: AngularPhoneHelperModule,
       providers: [
-        { provide: API_CONFIG_TOKEN, useValue: config },
+        { provide: API_CONFIG_TOKEN, useValue: config ?? defaultConfig },
         PhoneFormatPipe,
         PhoneFormatService,
       ],
